637a009138db01aa277567e908c45b4660f9df27
Tibor Tarnai <tibor.tarnai@sap.com>
2020-12-09 15:26:52 +0100

    interview

---
+++ src/test/java/com/sap/BirthDayCandlesTest.java
@@ -0,0 +1,22 @@
+package com.sap;
+
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class BirthDayCandlesTest {
+
+//    https://www.hackerrank.com/challenges/birthday-cake-candles/
+
+    static int birthdayCakeCandles(int[] ar) {
+        //TODO add solution here
+        return 0;
+    }
+
+
+    @Test
+    public void testA() {
+        assertThat(birthdayCakeCandles(new int[]{3, 1, 2, 3}), is(2));
+    }
+}
